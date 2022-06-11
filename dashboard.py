d13bce02e3dfa497e5ff5826bda4ab524862c36b
Eduardo Felix <77497147+DuFelix@users.noreply.github.com>
2022-06-10 22:21:47 -0300

    Atualizada versão com importação das bibliotecas

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+from black import read_cache
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import input, Output
+import dash_bootstrap_components as dbc
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+import numpy as np
+import pandas as pd
+import json
+
+df = pd.read_csv("HIST_PAINEL_COVIDBR_13mai2021.csv", sep=";")
+
