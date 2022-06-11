8a131aaf72f5278bd747728266e32fe907cf85ed
Eduardo Felix <77497147+DuFelix@users.noreply.github.com>
2022-06-10 22:40:06 -0300

    Criação de CSV

--- dashboard.py
+++ dashboard.py
@@ -12,5 +12,11 @@ import numpy as np
 import pandas as pd
 import json
 
-df = pd.read_csv("HIST_PAINEL_COVIDBR_13mai2021.csv", sep=";")
+# df = pd.read_csv("HIST_PAINEL_COVIDBR_13mai2021.csv", sep=";")
+# df_states = df[(~df["estado"].isna()) & (df["codmun"].isna())]
+# df_states
+# df_brasil = df[df["regiao"] == "Brasil"]
+# df_brasil
+# df_states.to_csv("df_states.csv")
+# df_brasil.to_csv("df_brasil.csv")
 
